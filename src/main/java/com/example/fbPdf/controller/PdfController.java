15100bf5c37d411e4bf2a1f42330631410717a03
anhnt39 <anhnt39@vnpay.vn>
2025-08-25 17:37:54 +0700

    update sign deferred using pdfbox

--- src/main/java/com/example/fbPdf/controller/PdfController.java
+++ src/main/java/com/example/fbPdf/controller/PdfController.java
@@ -1,7 +1,9 @@
 package com.example.fbPdf.controller;
 
+import com.example.fbPdf.enums.SigningProviderType;
 import com.example.fbPdf.event.StringPublisherService;
 import com.example.fbPdf.service.PdfService;
+import com.example.fbPdf.service.S3Service;
 import com.example.fbPdf.service.SigningService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -20,13 +22,31 @@ import java.util.Arrays;
 public class PdfController {
 
     private final PdfService pdfService;
+    private final S3Service s3Service;
     private final SigningService signingService;
     private final StringPublisherService stringPublisherService;
 
-    public PdfController(PdfService pdfService, SigningService signingService, StringPublisherService stringPublisherService) {
+    public PdfController(PdfService pdfService, S3Service s3Service, SigningService signingService, StringPublisherService stringPublisherService) {
         this.pdfService = pdfService;
+        this.s3Service = s3Service;
         this.signingService = signingService;
         this.stringPublisherService = stringPublisherService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) throws Exception {
+        String url = s3Service.uploadFile(file);
+        return ResponseEntity.ok(url);
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<byte[]> download(@PathVariable String filename) throws Exception {
+        byte[] content = s3Service.downloadFile(filename);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(content);
     }
 
     @GetMapping("/create")
@@ -53,14 +73,15 @@ public class PdfController {
 //    }
 
     @PostMapping("/sign")
-    public ResponseEntity<byte[]> signPdf(@RequestParam("file") MultipartFile file) throws Exception {
-        // Load PDF từ MultipartFile
+    public ResponseEntity<byte[]> signPdf(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam SigningProviderType signingType
+    ) throws Exception {
+
         InputStream inputStream = file.getInputStream();
 
-        // Gọi hàm ký PDF
-        byte[] signedPdf = signingService.signPdfIncremental(inputStream);
+        byte[] signedPdf = signingService.signPdf(inputStream, signingType);
 
-        // Trả về file PDF đã ký
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"signed.pdf\"")
                 .contentType(MediaType.APPLICATION_PDF)
